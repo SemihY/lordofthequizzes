bf0a2069698c987474e441b849c891454b1de47a
Muhammet Arıkaya <radiationhere@gmail.com>
2015-04-16 11:07:40 +0300

    initial commit

---
+++ js/core.js
@@ -0,0 +1,28 @@
+﻿$(document).on('ready', function () {
+
+
+    $(".topic").on('click', function () {
+      
+        var topic_name = $(this).find("h4").text();
+        var match = $(".match");
+        var match_topic = $(".match span.match-topic");
+       
+
+        console.log(topic_name);
+        match_topic.html(topic_name);
+        match.fadeIn("slow");
+    });
+
+
+    $(".match .play").on('click', function () {
+        $("#cancel").fadeIn("slow");
+        $(this).css({
+            'disabled' : true
+        });
+    });
+
+
+    $("#cancel").on('click', function () {
+        $(this).fadeOut("slow");
+    })
+});
